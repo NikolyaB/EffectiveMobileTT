b1ed76ebb42daac256d87aaeab7f5cb7ba97d5a6
Nikolay Bocharnikov <nikolay.bocharnikov@sebbia.com>
2024-06-03 18:52:40 +0300

    Initial Commit

---
+++ EffectiveMobileTT/EffectiveMobileTTApp.swift
@@ -0,0 +1,17 @@
+//
+//  EffectiveMobileTTApp.swift
+//  EffectiveMobileTT
+//
+//  Created by Nikolay Bocharnikov on 03.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct EffectiveMobileTTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
